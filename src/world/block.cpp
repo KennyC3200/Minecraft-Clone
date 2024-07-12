e806e4274d814fbaad2f8490e1fca8c1c891e52c
Kenny Chen <kennychen3200@gmail.com>
2024-07-12 21:23:03 +0800

    rendering a face

--- src/world/block.cpp
+++ src/world/block.cpp
@@ -22,10 +22,19 @@ void Block::init() {
             glm::vec2(0.0f, 0.0f), 
             glm::vec2(atlas.sprite_unit.x, atlas.sprite_unit.y))
     );
+    // --------------------------
+    // have BLOCK_GRASS side mesh
+    // --------------------------
+    blocks[BLOCK_DIRT] = Block(
+        BLOCK_DIRT,
+        BlockMesh(
+            glm::vec2(2 * atlas.sprite_unit.x, 0.0f),
+            glm::vec2(3 * atlas.sprite_unit.x, atlas.sprite_unit.y))
+    );
     blocks[BLOCK_STONE] = Block(
         BLOCK_STONE,
         BlockMesh(
-            glm::vec2(atlas.sprite_unit.x, 0.0f),
-            glm::vec2(2 * atlas.sprite_unit.x, atlas.sprite_unit.y))
+            glm::vec2(3 * atlas.sprite_unit.x, 0.0f),
+            glm::vec2(4 * atlas.sprite_unit.x, atlas.sprite_unit.y))
     );
 }
--- src/world/chunk.cpp
+++ src/world/chunk.cpp
@@ -22,13 +22,14 @@ void Chunk::render() {
     BlockMesh::shader.bind();
     BlockMesh::shader.uniform_texture_2d(Block::atlas.texture, 0);
 
-    float c = Block::atlas.sprite_unit.x;
+    glm::vec2 uv_max = Block::blocks[BLOCK_DIRT].mesh.uv_max;
+    glm::vec2 uv_min = Block::blocks[BLOCK_DIRT].mesh.uv_min;
 
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  c, 1.0f,  // top right
-         0.5f, -0.5f, 0.0f,  c, 1.0f - c,  // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f - c,  // bottom left
-        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f,  // top left 
+         0.5f,  0.5f, 0.0f,  uv_max.x, 1.0f - uv_min.y,  // top right
+         0.5f, -0.5f, 0.0f,  uv_max.x, 1.0f - uv_max.y,  // bottom right
+        -0.5f, -0.5f, 0.0f,  uv_min.x, 1.0f - uv_max.y,  // bottom left
+        -0.5f,  0.5f, 0.0f,  uv_min.x, 1.0f - uv_min.y,  // top left 
     };
     unsigned int indices[] = {
         0, 1, 3,  // first Triangle
