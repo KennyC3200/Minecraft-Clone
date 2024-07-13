ea78df51f426cbbf9bde94ba7e67c1af8e0ad4c9
Kenny Chen <kennychen3200@gmail.com>
2024-07-13 22:38:56 +0800

    lkajdfklda

--- src/world/block_mesh.hpp
+++ src/world/block_mesh.hpp
@@ -9,18 +9,6 @@ public:
     ~BlockMesh();
     BlockMesh(glm::vec2 uv_min, glm::vec2 uv_max);
 
-    enum Face {
-        FACE_UP = 0,
-        FACE_DOWN,
-        FACE_FRONT,
-        FACE_BACK,
-        FACE_LEFT,
-        FACE_RIGHT
-    };
-
-    void render(bool faces[6]);
-    void render_face(BlockMesh::Face face);
-
     static Shader shader;
 
     glm::vec2 uv_min, uv_max;
