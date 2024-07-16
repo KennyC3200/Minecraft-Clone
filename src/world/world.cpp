a5095ca4930a315580b5a647578ff87c90a8f16b
Kenny Chen <kennychen3200@gmail.com>
2024-07-16 19:56:57 +0800

    nvm bug is back

--- src/world/world.cpp
+++ src/world/world.cpp
@@ -6,9 +6,10 @@ void World::init(Player *player) {
     Block::init();
     Chunk::init(player);
 
-    chunks_count = 1;
+    chunks_count = 2;
     chunks = new Chunk*[chunks_count];
     chunks[0] = new Chunk(glm::vec<3, int>(0, 0, 0));
+    chunks[1] = new Chunk(glm::vec<3, int>(1, 0, 0));
 }
 
 void World::destroy() {
