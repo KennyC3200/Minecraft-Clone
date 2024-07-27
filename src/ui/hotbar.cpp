1793c43ea4639210d7ceee244fe51a53d042ad0c
Kenny Chen <kennychen3200@gmail.com>
2024-07-27 11:23:03 +0800

    hotbar without rendering block faces works

--- src/ui/hotbar.cpp
+++ src/ui/hotbar.cpp
@@ -14,10 +14,56 @@ void Hotbar::render() {
 
     UIComponent::shader.uniform_texture_2d(atlas.texture, 0);
     for (int i = 0; i < HOTBAR_SIZE; i++) {
-        // have vertices and indices
-        // buffer the uv coordinates separately
+        float u_x = 1.0f / window->size.x,
+              u_y = 1.0f / window->size.y;
+        float w = u_x * 80,
+              h = u_y * 80;
+        float x1 = -w * (HOTBAR_SIZE / 2.0f - i),
+              x2 = x1 + w;
+        float y1 = -1.0f + 0.1f,
+              y2 = y1 + h;
+
+        std::vector<float> vertices = {
+            x1, y2,
+            x1, y1,
+            x2, y1,
+            x2, y2,
+        };
+
+        unsigned int indices[] = {
+            0, 1, 3,
+            1, 2, 3,
+        };
+
         if (i == player->hotbar_idx) {
+            vertices.push_back(atlas.sprite_uv({2, 0}).x);
+            vertices.push_back(atlas.sprite_uv({2, 0}).y);
+            vertices.push_back(atlas.sprite_uv({2, 1}).x);
+            vertices.push_back(atlas.sprite_uv({2, 1}).y);
+            vertices.push_back(atlas.sprite_uv({1, 1}).x);
+            vertices.push_back(atlas.sprite_uv({1, 1}).y);
+            vertices.push_back(atlas.sprite_uv({1, 0}).x);
+            vertices.push_back(atlas.sprite_uv({1, 0}).y);
         } else {
+            vertices.push_back(atlas.sprite_uv({1, 0}).x);
+            vertices.push_back(atlas.sprite_uv({1, 0}).y);
+            vertices.push_back(atlas.sprite_uv({1, 1}).x);
+            vertices.push_back(atlas.sprite_uv({1, 1}).y);
+            vertices.push_back(atlas.sprite_uv({0, 1}).x);
+            vertices.push_back(atlas.sprite_uv({0, 1}).y);
+            vertices.push_back(atlas.sprite_uv({0, 0}).x);
+            vertices.push_back(atlas.sprite_uv({0, 0}).y);
         }
+
+        UIComponent::ibo.buffer(sizeof(indices), indices);
+        UIComponent::vbo.buffer(vertices.size() * sizeof(float), &vertices[0]);
+
+        UIComponent::vao.attr(UIComponent::vbo, 0, 2, GL_FLOAT, 0, 0);
+        UIComponent::vao.attr(UIComponent::vbo, 1, 2, GL_FLOAT, 0, (4 * 2) * sizeof(float));
+
+        UIComponent::vao.bind();
+        UIComponent::ibo.bind();
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
     }
 }
