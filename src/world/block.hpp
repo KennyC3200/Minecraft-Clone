dca0bed232936eb0209dbe17711bc763061f5016
Kenny Chen <kennychen3200@gmail.com>
2024-08-03 07:47:51 -0400

    using BLOCK_ID_GET macro for block id's but chunk mesh generating needs serious refactoring and some other tiny things also need serious refactoring

--- src/world/block.hpp
+++ src/world/block.hpp
@@ -12,18 +12,19 @@ typedef uint64_t BlockData;
 // - 4 bits light intensity
 // - 16 bits block id
 
-#define BLOCK_DATA_ID_GET(data) (data & 0xffff)
-#define BLOCK_DATA_ID_SET(data, id) (data = (data & 0xffffffffffff0000) + (uint64_t) id)
-#define BLOCK_DATA_LIGHT_GET(data) ((data >> 16) & 0xf)
-#define BLOCK_DATA_LIGHT_SET(data, intensity) (data = (data & 0xfffffffffff0ffff) + (uint64_t) (intensity << 16))
-#define BLOCK_DATA_RGB_GET(data) ((data >> 20) & 0xfff)
-#define BLOCK_DATA_RGB_SET(data, rgb) (data = (data & 0xffffffff000fffff) + (uint64_t) (rgb << 20))
-#define BLOCK_DATA_R_GET(data) ((data >> 20) & 0xf)
-#define BLOCK_DATA_R_SET(data, r) (data = (data & 0xffffffffff0fffff) + (uint64_t) (r << 20))
-#define BLOCK_DATA_G_GET(data) ((data >> 24) & 0xf)
-#define BLOCK_DATA_G_SET(data, g) (data = (data & 0xfffffffff0ffffff) + (uint64_t) (g << 24))
-#define BLOCK_DATA_B_GET(data) ((data >> 28) & 0xf)
-#define BLOCK_DATA_B_SET(data, g) (data = (data & 0xffffffff0fffffff) + (uint64_t) (b << 28))
+// TODO: refactor this mess
+#define BLOCK_ID_GET(data) (data & 0xffff)
+#define BLOCK_ID_SET(data, id) (data = (data & 0xffffffffffff0000) + (uint64_t) id)
+#define BLOCK_LIGHT_GET(data) ((data >> 16) & 0xf)
+#define BLOCK_LIGHT_SET(data, intensity) (data = (data & 0xfffffffffff0ffff) + (uint64_t) (intensity << 16))
+#define BLOCK_RGB_GET(data) ((data >> 20) & 0xfff)
+#define BLOCK_RGB_SET(data, rgb) (data = (data & 0xffffffff000fffff) + (uint64_t) (rgb << 20))
+#define BLOCK_R_GET(data) ((data >> 20) & 0xf)
+#define BLOCK_R_SET(data, r) (data = (data & 0xffffffffff0fffff) + (uint64_t) (r << 20))
+#define BLOCK_G_GET(data) ((data >> 24) & 0xf)
+#define BLOCK_G_SET(data, g) (data = (data & 0xfffffffff0ffffff) + (uint64_t) (g << 24))
+#define BLOCK_B_GET(data) ((data >> 28) & 0xf)
+#define BLOCK_B_SET(data, g) (data = (data & 0xffffffff0fffffff) + (uint64_t) (b << 28))
 
 enum BlockID {
     BLOCK_NONE = -1,
--- tests/block_data.cpp
+++ tests/block_data.cpp
@@ -2,8 +2,8 @@
 
 int main() {
     BlockData block = 0xff1232131f213213;
-    BLOCK_DATA_ID_SET(block, BLOCK_PLANK);
-    BLOCK_DATA_LIGHT_SET(block, 15);
-    std::cout << BLOCK_DATA_ID_GET(block) << std::endl;
-    std::cout << BLOCK_DATA_LIGHT_GET(block) << std::endl;
+    BLOCK_ID_SET(block, BLOCK_PLANK);
+    BLOCK_LIGHT_SET(block, 15);
+    std::cout << BLOCK_ID_GET(block) << std::endl;
+    std::cout << BLOCK_LIGHT_GET(block) << std::endl;
 }
