69bfadc63334435ed4fa8a60107ffd2156fc3c88
Kenny Chen <kennychen3200@gmail.com>
2024-08-05 07:43:04 -0400

    fixed and refactored the BlockData macros

--- src/world/block.hpp
+++ src/world/block.hpp
@@ -12,19 +12,23 @@
 // - 16 bits block id
 typedef uint64_t BlockData;
 
-// TODO: refactor this mess--couldn't get any uglier but idk
-#define BLOCK_ID_GET(data) (data & 0xffff)
-#define BLOCK_ID_SET(data, id) (data = (data & 0xffffffffffff0000) + (uint64_t) id)
-#define BLOCK_LIGHT_GET(data) ((data >> 16) & 0xf)
-#define BLOCK_LIGHT_SET(data, intensity) (data = (data & 0xfffffffffff0ffff) + (uint64_t) (intensity << 16))
-#define BLOCK_RGB_GET(data) ((data >> 20) & 0xfff)
-#define BLOCK_RGB_SET(data, rgb) (data = (data & 0xffffffff000fffff) + (uint64_t) (rgb << 20))
-#define BLOCK_R_GET(data) ((data >> 20) & 0xf)
-#define BLOCK_R_SET(data, r) (data = (data & 0xffffffffff0fffff) + (uint64_t) (r << 20))
-#define BLOCK_G_GET(data) ((data >> 24) & 0xf)
-#define BLOCK_G_SET(data, g) (data = (data & 0xfffffffff0ffffff) + (uint64_t) (g << 24))
-#define BLOCK_B_GET(data) ((data >> 28) & 0xf)
-#define BLOCK_B_SET(data, g) (data = (data & 0xffffffff0fffffff) + (uint64_t) (b << 28))
+#define BLOCK_ID_SET(data, id)               (data = (data & 0xffffffffffff0000) + (uint64_t) id)
+#define BLOCK_LIGHT_SET(data, intensity)     (data = (data & 0xfffffffffff0ffff) + (uint64_t) (intensity << 16))
+#define BLOCK_RGB_SET(data, rgb)             (data = (data & 0xffffffff000fffff) + (uint64_t) (rgb << 28))
+#define BLOCK_R_SET(data, r)                 (data = (data & 0xffffffff0fffffff) + (uint64_t) (r << 28))
+#define BLOCK_G_SET(data, g)                 (data = (data & 0xfffffffff0ffffff) + (uint64_t) (g << 24))
+#define BLOCK_B_SET(data, g)                 (data = (data & 0xffffffffff0fffff) + (uint64_t) (b << 20))
+#define BLOCK_SUNLIGHT_SET(data, intensity)  (data = (data & 0xfffffff0ffffffff) + (uint64_t) (light << 32))
+#define BLOCK_METADATA_SET(data, metadata)   (data = (data & 0x0000000fffffffff) + (uint64_t) (metadata << 60))
+
+#define BLOCK_ID_GET(data)       (data & 0xffff)
+#define BLOCK_LIGHT_GET(data)    ((data >> 16) & 0xf)
+#define BLOCK_RGB_GET(data)      ((data >> 20) & 0xfff)
+#define BLOCK_R_GET(data)        ((data >> 28) & 0xf)
+#define BLOCK_G_GET(data)        ((data >> 24) & 0xf)
+#define BLOCK_B_GET(data)        ((data >> 20) & 0xf)
+#define BLOCK_SUNLIGHT_GET(data) ((data >> 32) & 0xf)
+#define BLOCK_METADATA_GET(data) ((data >> 36) & 0xfffffff)
 
 enum BlockID {
     BLOCK_NONE = -1,
